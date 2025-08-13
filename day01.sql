ab035e8be11457270d45be354afc6326f510cc20
Walid Sharaar <walidsharar@gmail.com>
2025-08-14 00:25:31 +0200

    Update day01.sql

--- day01.sql
+++ day01.sql
@@ -1,3 +1,10 @@
 /*
 Calculates the difference between the highest salaries in the marketing and engineering departments. Output just the absolute difference in salaries.
 */
+
+select
+abs(max(case when d.department = 'marketing' then e.salary end )  -
+max(case when d.department = 'engineering' then e.salary end)) as salary_difference
+from db_employee e
+left join db_dept d
+on e.department_id=d.id
