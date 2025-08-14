05eaf7dc22fa9549bb08ea0f694de9c9e7b3dbe6
Walid Sharaar <walidsharar@gmail.com>
2025-08-14 23:56:25 +0200

    Update day02.sql

--- day02.sql
+++ day02.sql
@@ -3,3 +3,17 @@ We have a table with employees and their salaries, however, some of the records 
 assuming that salaries increase each year. Output their id, first name, 
 last name, department ID, and current salary. Order your list by employee ID in ascending order.
 */
+
+WITH cte AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY id
+            ORDER BY salary DESC
+        ) AS rk
+    FROM ms_employee_salary
+)
+SELECT *
+FROM cte
+WHERE rk = 1;
+
