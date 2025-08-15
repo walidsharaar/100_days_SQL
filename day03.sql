17d5bb960844d5f46f433e47ee0a19faaea4835a
Walid Sharaar <walidsharar@gmail.com>
2025-08-15 21:25:55 +0200

    Update day03.sql

--- day03.sql
+++ day03.sql
@@ -1,3 +1,7 @@
 /*
 Find the job titles of the employees with the highest salary. If multiple employees have the same highest salary, include the job titles for all such employees.
 */
+
+select t.worker_title as best_paid_title from worker w
+join title t on w.worker_id = t.worker_ref_id
+where  w. salary = (select max(salary) from worker) 
