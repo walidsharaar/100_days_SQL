0d71c10fb897ab2013a18ed725f3f4e6aa21cbe8
Walid Sharaar <walidsharar@gmail.com>
2025-08-15 21:29:04 +0200

    Update day03.sql

--- day03.sql
+++ day03.sql
@@ -5,3 +5,29 @@ Find the job titles of the employees with the highest salary. If multiple employ
 select t.worker_title as best_paid_title from worker w
 join title t on w.worker_id = t.worker_ref_id
 where  w. salary = (select max(salary) from worker) 
+
+
+--Alternative
+--1.
+WITH max_salary AS (
+    SELECT MAX(salary) AS salary
+    FROM worker
+)
+SELECT t.worker_title AS best_paid_title
+FROM worker w
+JOIN title t 
+  ON w.worker_id = t.worker_ref_id
+JOIN max_salary m
+  ON w.salary = m.salary;
+
+--2.
+SELECT worker_title AS best_paid_title
+FROM (
+    SELECT
+        t.worker_title,
+        RANK() OVER (ORDER BY w.salary DESC) AS rnk
+    FROM worker w
+    JOIN title t
+      ON w.worker_id = t.worker_ref_id
+) ranked
+WHERE rnk = 1;
