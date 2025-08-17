dd78ca91738f698d34da290d5409697f9040f417
Walid Sharaar <walidsharar@gmail.com>
2025-08-17 20:53:59 +0200

    Update Day05.sql

--- Day05.sql
+++ Day05.sql
@@ -2,3 +2,10 @@
 Compare each employee's salary with the average salary of the corresponding department.
 Output the department, first name, and salary of employees along with the average salary of that department.
 */
+with cte as (
+select department,avg(salary) as avg_salary
+from employee
+group by department)
+select e.department,e.first_name,e.salary, avg_salary from cte c
+join employee e on c.department = e.department
+order by department asc
