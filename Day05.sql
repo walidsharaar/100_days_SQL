6d8b3af18bbf18949283172c45e45a6d58624bf9
Walid Sharaar <walidsharar@gmail.com>
2025-08-17 20:46:43 +0200

    Create Day05.sql

---
+++ Day05.sql
@@ -0,0 +1,4 @@
+/*
+Compare each employee's salary with the average salary of the corresponding department.
+Output the department, first name, and salary of employees along with the average salary of that department.
+*/
