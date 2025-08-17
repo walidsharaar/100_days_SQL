16e0daa243b707ced81e77ae048ee79b8df29701
Walid Sharaar <walidsharar@gmail.com>
2025-08-17 20:42:51 +0200

    Update Day04.sql

--- Day04.sql
+++ Day04.sql
@@ -1,3 +1,7 @@
 /*
 Find the last time each bike was in use. Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). Order the results by bikes that were most recently used.
 */
+
+select bike_number, max(end_time) as last_used from dc_bikeshare_q1_2012
+group by bike_number
+order by max(end_time) desc
