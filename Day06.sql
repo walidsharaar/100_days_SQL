2cd6a823c8b8f28f25bfd3379b5b33f8cfb04ce5
Walid Sharaar <walidsharar@gmail.com>
2025-08-18 09:18:46 +0200

    Create Day06.sql

---
+++ Day06.sql
@@ -0,0 +1,5 @@
+/*
+Find the details of each customer regardless of whether the customer made an order. Output the customer's first name, last name, and the city along with the order details.
+Sort records based on the customer's first name and the order details in ascending order.
+
+*/
