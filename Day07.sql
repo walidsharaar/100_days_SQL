d61c76eb3748e126e4affd8def387bfd3f87184d
Walid Sharaar <walidsharar@gmail.com>
2025-08-19 09:33:55 +0200

    Update Day07.sql

--- Day07.sql
+++ Day07.sql
@@ -1,3 +1,10 @@
 /*
 Find the average number of bathrooms and bedrooms for each city’s property types. Output the result along with the city name and the property type.
 */
+SELECT
+  city,
+  property_type,
+  ROUND(AVG(bathrooms), 2) AS avg_bathrooms,
+  ROUND(AVG(bedrooms), 2) AS avg_bedrooms
+FROM airbnb_search_details
+GROUP BY city, property_type;
