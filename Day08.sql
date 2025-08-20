3dd3d5caf87a881545c3816d557d93ae510c9129
Walid Sharaar <walidsharar@gmail.com>
2025-08-20 18:48:50 +0200

    Update Day08.sql

--- Day08.sql
+++ Day08.sql
@@ -3,3 +3,10 @@ Find the number of workers by department who joined on or after April 1, 2014.
 Output the department name along with the corresponding number of workers.
 Sort the results based on the number of workers in descending order.
 */
+
+
+
+select department, count(worker_id)
+from worker
+where joining_date>'2014-04-01'
+group by department
