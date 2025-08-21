f3a5abc02dc3199e346f01023ca7c788b4a6e81e
Walid Sharaar <walidsharar@gmail.com>
2025-08-21 14:29:48 +0200

    Update Day09.sql

--- Day09.sql
+++ Day09.sql
@@ -1,3 +1,11 @@
 /*
 Write a query that returns the number of unique users per client for each month. Assume all events occur within the same year, so only the month needs to be in the output as a number from 1 to 12.
 */
+
+SELECT 
+    client_id,
+    EXTRACT(MONTH FROM time_id) AS month,
+    COUNT(user_id) AS users_num
+FROM fact_events
+GROUP BY client_id, month
+ORDER BY client_id, month;
