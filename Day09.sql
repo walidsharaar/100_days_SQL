b4c42ac95454f6ff43bd704a49d0181840311cb5
Walid Sharaar <walidsharar@gmail.com>
2025-08-21 14:16:08 +0200

    Create Day09.sql

---
+++ Day09.sql
@@ -0,0 +1,3 @@
+/*
+Write a query that returns the number of unique users per client for each month. Assume all events occur within the same year, so only the month needs to be in the output as a number from 1 to 12.
+*/
