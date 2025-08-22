ed83835f80fdd5558bc00b0d739f646ab7d62407
Walid Sharaar <walidsharar@gmail.com>
2025-08-22 07:56:14 +0200

    Create Day10.sql

---
+++ Day10.sql
@@ -0,0 +1,3 @@
+/*
+Find the top 10 ranked songs by position. Output the track name along with the corresponding position and sort records by the position in descending order and track name alphabetically, as many tracks are tied for the same position.
+*/
