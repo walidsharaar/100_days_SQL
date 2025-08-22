de417fbc547fe08c1eb9f87bac2e9842a3c9c22b
Walid Sharaar <walidsharar@gmail.com>
2025-08-22 08:04:06 +0200

    Update Day10.sql

--- Day10.sql
+++ Day10.sql
@@ -1,3 +1,11 @@
 /*
-Find the top 10 ranked songs by position. Output the track name along with the corresponding position and sort records by the position in descending order and track name alphabetically, as many tracks are tied for the same position.
+Find songs that have ranked in the top position. Output the track name and the number of times it ranked at the top. Sort your records by the number of times the song was in the top position in descending order.
 */
+
+
+
+
+select trackname, count(position) as times_top1 from spotify_worldwide_daily_song_ranking
+where position = 1
+group by trackname
+order by times_top1 desc
