f9dfd4c49674b24a83d8d30b8797dbb7eee7fbfb
Walid Sharaar <walidsharar@gmail.com>
2025-08-25 09:28:04 +0200

    Update Day13.sql

--- Day13.sql
+++ Day13.sql
@@ -13,3 +13,18 @@ from facebook_employees fe
 left join facebook_hack_survey fh
 on fh.employee_id = fe.id
 group by fe.location
+
+--Alternative
+
+WITH survey_avg AS (
+    SELECT employee_id,
+           AVG(popularity) AS avg_popularity
+    FROM facebook_hack_survey
+    GROUP BY employee_id
+)
+SELECT fe.location,
+       AVG(sa.avg_popularity) AS location_popularity
+FROM facebook_employees fe
+LEFT JOIN survey_avg sa
+  ON sa.employee_id = fe.id
+GROUP BY fe.location;
