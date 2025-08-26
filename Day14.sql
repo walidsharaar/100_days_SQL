68f693923a19a6f72e048a1de06673fbc87d82e9
Walid Sharaar <walidsharar@gmail.com>
2025-08-26 17:48:07 +0200

    Update Day14.sql

--- Day14.sql
+++ Day14.sql
@@ -5,3 +5,9 @@ Output the order date, details and cost along with the first name.
 Order records based on the customer id in ascending order.
 
 */
+
+select c.first_name, o.order_date, o.order_details, total_order_cost
+from customers c
+left join orders o 
+on c.id = o.cust_id
+where first_name in ('Jill','Eva')
