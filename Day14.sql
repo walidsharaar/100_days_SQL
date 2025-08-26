c20dad3f2ed63be5ae2185047017624f301e62d5
Walid Sharaar <walidsharar@gmail.com>
2025-08-26 17:42:07 +0200

    Create Day14.sql

---
+++ Day14.sql
@@ -0,0 +1,7 @@
+/*
+Find order details made by Jill and Eva.
+Consider Jill and Eva as the first names of customers.
+Output the order date, details and cost along with the first name.
+Order records based on the customer id in ascending order.
+
+*/
