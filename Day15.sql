e5906f50f66776993fa5cc39c268e41eda6432e8
Walid Sharaar <walidsharar@gmail.com>
2025-08-27 09:08:20 +0200

    Create Day15.sql

---
+++ Day15.sql
@@ -0,0 +1,1 @@
+/*Write a query that will calculate the number of shipments per month. The unique key for one shipment is a combination of shipment_id and sub_id. Output the year_month in format YYYY-MM and the number of shipments in that month.*/
