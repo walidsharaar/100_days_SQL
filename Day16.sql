e8b9c7056a3945d4ec16a26915c0ef2d8651b823
Walid Sharaar <walidsharar@gmail.com>
2025-08-28 09:58:41 +0200

    Update Day16.sql

--- Day16.sql
+++ Day16.sql
@@ -3,3 +3,11 @@ Count the number of user events performed by MacBookPro users.
 Output the result along with the event name.
 Sort the result based on the event count in the descending order.
 */
+
+select distinct device from playbook_events;
+
+select event_name , count(user_id) as event_count
+from playbook_events
+where device='macbook pro'
+group by event_name
+order by count(user_id) desc
