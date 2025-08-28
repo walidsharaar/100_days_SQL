825c63d1d305a1b8b6392e5383a36a9c95d1022e
Walid Sharaar <walidsharar@gmail.com>
2025-08-28 09:59:58 +0200

    Update Day16.sql

--- Day16.sql
+++ Day16.sql
@@ -11,3 +11,28 @@ from playbook_events
 where device='macbook pro'
 group by event_name
 order by count(user_id) desc
+
+
+--Altenatives
+--1.
+WITH macbook_events AS (
+    SELECT event_name, user_id
+    FROM playbook_events
+    WHERE device = 'macbook pro'
+)
+SELECT 
+    event_name,
+    COUNT(user_id) AS event_count
+FROM macbook_events
+GROUP BY event_name
+ORDER BY event_count DESC;
+
+
+--2.
+SELECT DISTINCT
+    event_name,
+    COUNT(user_id) OVER (PARTITION BY event_name) AS event_count
+FROM playbook_events
+WHERE device = 'macbook pro'
+ORDER BY event_count DESC;
+
