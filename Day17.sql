cea0fae1fbde953de362cf2d2e94edba242870bc
Walid Sharaar <walidsharar@gmail.com>
2025-08-29 08:18:23 +0200

    Update Day17.sql

--- Day17.sql
+++ Day17.sql
@@ -1,1 +1,34 @@
+/*
+Find the most profitable company from the financial sector. Output the result along with the continent.
+*/
 
+
+
+
+select company,continent from forbes_global_2010_2014 
+where sector='Financials' and profits = (select max(profits) from forbes_global_2010_2014  )
+
+
+
+
+--Alternative
+--1.
+select company,continent,sum(profits) as total_profits from forbes_global_2010_2014 
+where sector='Financials'
+group by company,continent
+order by sum(profits) desc
+limit 1
+
+--2.
+WITH max_financials AS (
+    SELECT MAX(profits) AS max_profit
+    FROM forbes_global_2010_2014
+    WHERE sector = 'Financials'
+)
+SELECT company, continent
+FROM forbes_global_2010_2014 f
+JOIN max_financials m
+  ON f.profits = m.max_profit
+WHERE f.sector = 'Financials';
+
+--3.
