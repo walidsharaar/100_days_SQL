e6b13d68ee6305b73f7b2f4b41f9730e7df30627
Walid Sharaar <walidsharar@gmail.com>
2025-08-31 18:20:33 +0200

    Update Day19.sql

--- Day19.sql
+++ Day19.sql
@@ -9,3 +9,14 @@ Count the total number of violations that occurred at 'Roxanne Cafe' for each ye
 select extract(year from inspection_date) as inspection_year , count(violation_id) as n_violations from sf_restaurant_health_violations
 where business_name='Roxanne Cafe'
 group by inspection_year
+
+
+---Alternative
+SELECT 
+    EXTRACT(YEAR FROM inspection_date)::INT AS inspection_year,
+    EXTRACT(MONTH FROM inspection_date)::INT AS inspection_month,
+    COUNT(violation_id) AS n_violations
+FROM sf_restaurant_health_violations
+WHERE business_name = 'Roxanne Cafe'
+GROUP BY inspection_year, inspection_month
+ORDER BY inspection_year, inspection_month;
