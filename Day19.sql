4e0873852d47efd2e0bd87d54a7e968bd7ce0e77
Walid Sharaar <walidsharar@gmail.com>
2025-08-31 18:19:39 +0200

    Update Day19.sql

--- Day19.sql
+++ Day19.sql
@@ -4,3 +4,8 @@ You are given a dataset of health inspections that includes details about violat
 
 Count the total number of violations that occurred at 'Roxanne Cafe' for each year, based on the inspection date. Output the year and the corresponding number of violations in ascending order of the year.
 */
+
+
+select extract(year from inspection_date) as inspection_year , count(violation_id) as n_violations from sf_restaurant_health_violations
+where business_name='Roxanne Cafe'
+group by inspection_year
