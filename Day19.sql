109c96dff8f8653413a2c2fe95218b47e5ef31ec
Walid Sharaar <walidsharar@gmail.com>
2025-08-31 18:15:32 +0200

    Create Day19.sql

---
+++ Day19.sql
@@ -0,0 +1,6 @@
+/*
+You are given a dataset of health inspections that includes details about violations. Each row represents an inspection, and if an inspection resulted in a violation, the violation_id column will contain a value.
+
+
+Count the total number of violations that occurred at 'Roxanne Cafe' for each year, based on the inspection date. Output the year and the corresponding number of violations in ascending order of the year.
+*/
