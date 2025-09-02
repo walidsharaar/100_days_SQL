7e7b2eec54c446965e18251fffb8c1051699442d
Walid Sharaar <walidsharar@gmail.com>
2025-09-02 09:05:17 +0200

    Update Day21.sql

--- Day21.sql
+++ Day21.sql
@@ -4,3 +4,19 @@ Find the customers with the highest daily total order cost between 2019-02-01 an
 
 For simplicity, you can assume that every first name in the dataset is unique.
 */
+
+SELECT * FROM customers;
+SELECT * FROM orders
+
+SELECT 
+    c.first_name,
+    o.order_date,
+    SUM(o.total_order_cost) AS total_cost
+FROM customers c
+JOIN orders o 
+  ON c.id = o.cust_id
+WHERE o.order_date BETWEEN '2019-02-01' AND '2019-05-01'
+GROUP BY c.first_name, o.order_date
+ORDER BY total_cost DESC
+LIMIT 1;
+
