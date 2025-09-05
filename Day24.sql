a2374bb4c4101c5e93bbde6e8c3fa364e5221eec
Walid Sharaar <walidsharar@gmail.com>
2025-09-05 07:37:08 +0200

    Create Day24.sql

---
+++ Day24.sql
@@ -0,0 +1,5 @@
+/*
+Calculate the friend acceptance rate for each date when friend requests were sent. A request is sent if action = 'sent', and accepted if action = 'accepted'. If a request is not accepted, it will not be recorded in the table as accepted. 
+The output will only include dates where requests were sent and at least one of them was accepted, 
+as the acceptance rate can only be calculated for those dates. Show the results ordered from the earliest to the latest date.
+*/
