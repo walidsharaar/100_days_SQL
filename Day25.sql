b9bb835f6e6f9188bcc983d9063428501b869fdc
Walid Sharaar <walidsharar@gmail.com>
2025-09-06 22:32:45 +0200

    Create Day25.sql

---
+++ Day25.sql
@@ -0,0 +1,3 @@
+/*Identify returning active users by finding users who made a second purchase within 1 to 7 days after their first purchase. Ignore same-day purchases. 
+Output a list of these user_ids.
+*/
