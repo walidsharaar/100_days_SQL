595a51091b650705804af5c5059db6c3057ee5e9
Walid Sharaar <walidsharar@gmail.com>
2025-09-09 13:19:28 +0200

    Create Day28.sql

---
+++ Day28.sql
@@ -0,0 +1,9 @@
+/*
+Find the email activity rank for each user. Email activity rank is defined by the total number of emails sent. The user with the highest number of emails sent will have a rank of 1, and so on. Output the user, total emails,
+and their activity rank.
+
+
+•	Order records first by the total emails in descending order.
+•	Then, sort users with the same number of emails in alphabetical order by their username.
+•	In your rankings, return a unique value (i.e., a unique rank) even if multiple users have the same number of emails.
+*/
