54fe0fcb47de7fc83504eb06a4837f1bcb6010b0
Walid Sharaar <walidsharar@gmail.com>
2025-09-10 10:57:32 +0200

    Create Day29.sql

---
+++ Day29.sql
@@ -0,0 +1,5 @@
+/*
+Find the processed rate of tickets for each type. The processed rate is defined as the number of processed tickets divided by the total number of tickets for that type. Round this result to two decimal places.
+
+
+*/
