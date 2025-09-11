e915908d1b77b07758d321fc350f3eeae9d17450
Walid Sharaar <walidsharar@gmail.com>
2025-09-11 08:57:53 +0200

    Create Day30.sql

---
+++ Day30.sql
@@ -0,0 +1,4 @@
+/*
+Calculate the total revenue from each customer in March 2019. Include only customers who were active in March 2019. An active user is a customer who made at least one transaction in March 2019.
+Output the revenue along with the customer id and sort the results based on the revenue in descending order.
+*/
