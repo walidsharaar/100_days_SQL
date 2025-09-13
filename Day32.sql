44386424f2d212a5547e29a19499ffe845d45dcf
Walid Sharaar <walidsharar@gmail.com>
2025-09-13 20:45:55 +0200

    Create Day32.sql

---
+++ Day32.sql
@@ -0,0 +1,13 @@
+/*
+Make a report showing the number of survivors and non-survivors by passenger class. Classes are categorized based on the pclass value as:
+
+
+•	First class: pclass = 1
+•	Second class: pclass = 2
+•	Third class: pclass = 3
+
+
+Output the number of survivors and non-survivors by each class.
+
+
+*/
