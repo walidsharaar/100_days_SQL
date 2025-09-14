5633470a08b8bcaa30543926ca1d435c0153cd0d
Walid Sharaar <walidsharar@gmail.com>
2025-09-14 09:10:25 +0200

    Update Day33.sql

--- Day33.sql
+++ Day33.sql
@@ -7,3 +7,18 @@ select salary from employee
 where salary < (select max(salary) from employee)
 order by salary desc
 limit 1
+
+
+--Alternatives
+
+select distinct salary
+from employee
+order by salary desc
+offset 1 limit 1;
+---
+select salary
+from (
+    select salary, dense_rank() over (order by salary desc) as rnk
+    from employee
+) t
+where rnk = 2;
