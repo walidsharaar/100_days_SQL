aa1fc8613cc6bb7a83b90391aac5d2a1c21579e0
Walid Sharaar <walidsharar@gmail.com>
2025-09-15 09:35:21 +0200

    Update Day34.sql

--- Day34.sql
+++ Day34.sql
@@ -1,3 +1,13 @@
 /*
 Find employees who are earning more than their managers. Output the employee's first name along with the corresponding salary.
 */
+
+SELECT 
+    e.first_name AS employee_name,
+    e.salary     AS employee_salary,
+    m.first_name AS manager_name,
+    m.salary     AS manager_salary
+FROM employee e
+INNER JOIN employee m 
+    ON e.manager_id = m.id
+WHERE e.salary > m.salary;
