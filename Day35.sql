5e725f2368793baee9f5cd8805d60637c6cf6938
Walid Sharaar <walidsharar@gmail.com>
2025-09-16 09:15:02 +0200

    Update Day35.sql

--- Day35.sql
+++ Day35.sql
@@ -2,3 +2,17 @@
 Find the employee with the highest salary per department.
 Output the department name, employee's first name along with the corresponding salary.
 */
+
+
+SELECT
+department,
+first_name,
+salary
+FROM employee
+WHERE (department, salary) IN(
+SELECT 
+department,
+MAX(salary)
+FROM employee
+GROUP BY department
+)
