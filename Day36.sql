234a59ff0b58c0994aff9385c22090c781802b79
Walid Sharaar <walidsharar@gmail.com>
2025-09-17 13:14:24 +0200

    Update Day36.sql

--- Day36.sql
+++ Day36.sql
@@ -11,3 +11,28 @@ select  first_name, target
 from salesforce_employees
 where manager_id=13 and 
 target=(select max(target) from salesforce_employees where manager_id=13)
+
+
+--Alternative
+--1.
+SELECT first_name, target
+FROM (
+    SELECT 
+        first_name,
+        target,
+        RANK() OVER (PARTITION BY manager_id ORDER BY target DESC) AS rnk
+    FROM salesforce_employees
+    WHERE manager_id = 13
+) t
+WHERE rnk = 1;
+
+
+--2.
+SELECT first_name, target
+FROM salesforce_employees
+WHERE manager_id = 13
+  AND target = (
+      SELECT MAX(target)
+      FROM salesforce_employees
+      WHERE manager_id = 13
+  );
