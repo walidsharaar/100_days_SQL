c551feb4122069c3614c3865df03dc578bf7b77a
Walid Sharaar <walidsharar@gmail.com>
2025-09-17 13:13:01 +0200

    Create Day36.sql

---
+++ Day36.sql
@@ -0,0 +1,13 @@
+/*
+
+Identify the employee(s) working under manager manager_id=13 who have achieved the highest target. Return each such employee’s first name alongside the target value.
+The goal is to display the maximum target among all employees under manager_id=13 and show which employee(s) reached that top value.
+*/
+
+select * from salesforce_employees;
+
+
+select  first_name, target
+from salesforce_employees
+where manager_id=13 and 
+target=(select max(target) from salesforce_employees where manager_id=13)
