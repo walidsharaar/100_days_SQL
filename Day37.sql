9041bc8c1b388b9bd2e3a7ded7606cede7786e94
Walid Sharaar <walidsharar@gmail.com>
2025-09-19 22:21:00 +0200

    Create Day37.sql

---
+++ Day37.sql
@@ -0,0 +1,4 @@
+/*
+Find the Olympics with the highest number of unique athletes. The Olympics game is a combination of the year and the season, and is found in the games column. Output the Olympics along with the corresponding number of athletes. 
+The id column uniquely identifies an athlete.
+*/
