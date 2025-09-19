49ff0fd656757d184eef59fed9dad2998ec33f46
Walid Sharaar <walidsharar@gmail.com>
2025-09-19 22:26:43 +0200

    Create Day38.sql

---
+++ Day38.sql
@@ -0,0 +1,10 @@
+/*
+Find the top 5 businesses with most reviews. Assume that each row has a unique business_id such that the total reviews for each business is listed on each row. Output the business name along with the total number of reviews
+and order your results by the total reviews in descending order.
+
+
+If there are ties in review counts, businesses with the same number of reviews receive the same rank, and subsequent ranks are skipped accordingly (e.g., if two businesses tie for rank 4, the next business receives rank 6, 
+skipping rank 5).
+
+
+*/
