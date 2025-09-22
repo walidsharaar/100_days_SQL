9abc44511fc46053f2195a992e406c21883f3aef
Walid Sharaar <walidsharar@gmail.com>
2025-09-22 09:05:18 +0200

    Create Day41.sql

---
+++ Day41.sql
@@ -0,0 +1,4 @@
+/*
+Find the average total compensation based on employee titles and gender. Total compensation is calculated by adding both the salary and bonus of each employee. However, not every employee receives a bonus so disregard employees without bonuses in your calculation. Employee can receive more than one bonus.
+Output the employee title, gender (i.e., sex), along with the average total compensation.
+*/
