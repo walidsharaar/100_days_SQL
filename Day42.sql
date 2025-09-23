ffb0e071e08355fc88692dea3fa06f0599d95f8d
Walid Sharaar <walidsharar@gmail.com>
2025-09-23 18:51:31 +0200

    Update Day42.sql

--- Day42.sql
+++ Day42.sql
@@ -2,3 +2,11 @@
 Find matching hosts and guests pairs in a way that they are both of the same gender and nationality.
 Output the host id and the guest id of matched pair.
 */
+SELECT 
+DISTINCT
+    h.host_id, 
+    g.guest_id
+FROM airbnb_hosts AS h
+JOIN airbnb_guests AS g
+  ON h.gender = g.gender
+ AND h.nationality = g.nationality;
