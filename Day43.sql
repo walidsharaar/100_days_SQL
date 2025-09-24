1556e7b0528d54c4392333fbb0bfabf782850fd6
Walid Sharaar <walidsharar@gmail.com>
2025-09-24 09:48:20 +0200

    Update Day43.sql

--- Day43.sql
+++ Day43.sql
@@ -2,3 +2,11 @@
 Find the percentage of shipable orders.
 Consider an order is shipable if the customer's address is known.
 */
+
+
+SELECT 
+    100 * (AVG(CASE WHEN address IS NOT NULL THEN 1 ELSE 0 END)) AS percent_shipable
+FROM orders AS o
+LEFT JOIN customers AS c
+ON o.cust_id = c.id
+;
