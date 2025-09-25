7fc621d397142fbc1fd421687df4ae78f37dd611
Walid Sharaar <walidsharar@gmail.com>
2025-09-25 10:50:54 +0200

    Create Day44.sql

---
+++ Day44.sql
@@ -0,0 +1,4 @@
+/*
+Calculate the percentage of spam posts in all viewed posts by day. A post is considered a spam if a string "spam" is inside keywords of the post. Note that the facebook_posts table stores all posts posted by users. 
+The facebook_post_views table is an action table denoting if a user has viewed a post.
+*/
