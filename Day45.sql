13d26e3714081abc903f3044d84bf26c390d7795
Walid Sharaar <walidsharar@gmail.com>
2025-09-26 09:03:19 +0200

    Create Day45.sql

---
+++ Day45.sql
@@ -0,0 +1,7 @@
+/*
+We’re analyzing user data to understand how popular Apple devices are among users who have performed at least one event on the platform. Specifically, we want to measure this popularity across different languages. 
+Count the number of distinct users using Apple devices —limited to "macbook pro", "iphone 5s", and "ipad air" — and compare it to the total number of users per language.
+
+
+Present the results with the language, the number of Apple users, and the total number of users for each language. Finally, sort the results so that languages with the highest total user count appear first.
+*/
