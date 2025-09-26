d8d8b2039f49094226c3b0a26be0213d57c52264
Walid Sharaar <walidsharar@gmail.com>
2025-09-26 09:09:53 +0200

    Update Day45.sql

--- Day45.sql
+++ Day45.sql
@@ -5,3 +5,21 @@ Count the number of distinct users using Apple devices —limited to "macbook pr
 
 Present the results with the language, the number of Apple users, and the total number of users for each language. Finally, sort the results so that languages with the highest total user count appear first.
 */
+
+
+WITH event_agg AS (
+    SELECT 
+        user_id,
+        COUNT(DISTINCT CASE WHEN device IN ('macbook pro','iphone 5s','ipad air') THEN user_id END) AS apple_flag,
+        1 AS total_flag
+    FROM playbook_events
+    GROUP BY user_id
+)
+SELECT 
+    b.language,
+    SUM(apple_flag) AS apple,
+    SUM(total_flag) AS total
+FROM event_agg e
+JOIN playbook_users b USING(user_id)
+GROUP BY b.language
+ORDER BY total DESC;
