e5263649d84a078848245a9a2fb92d4298666e01
Walid Sharaar <walidsharar@gmail.com>
2025-09-27 09:08:37 +0200

    Create Day46.sql

---
+++ Day46.sql
@@ -0,0 +1,14 @@
+/*
+Identify customers who did not place an order between 2019-02-01 and 2019-03-01.
+
+
+Include:
+
+
+•    Customers who placed orders only outside this date range.
+•    Customers who never placed any orders.
+
+
+Output the customers' first names.
+*/
+
