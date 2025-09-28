18c9031734931b02276238d7e1f76360e98bea3a
Walid Sharaar <walidsharar@gmail.com>
2025-09-28 12:23:18 +0200

    Create Day47.sql

---
+++ Day47.sql
@@ -0,0 +1,5 @@
+/*
+We have data on rental properties and their owners. Write a query that figures out how many different apartments (use unit_id) are owned by people under 30, broken down by their nationality. 
+We want to see which nationality owns the most apartments, 
+so make sure to sort the results accordingly.
+*/
