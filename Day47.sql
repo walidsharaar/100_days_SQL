7fef280c66afe5e79c697f302f16bba05d9f8fe6
Walid Sharaar <walidsharar@gmail.com>
2025-09-28 12:31:03 +0200

    Update Day47.sql

--- Day47.sql
+++ Day47.sql
@@ -3,3 +3,15 @@ We have data on rental properties and their owners. Write a query that figures o
 We want to see which nationality owns the most apartments, 
 so make sure to sort the results accordingly.
 */
+
+SELECT 
+    h.nationality,
+    COUNT(DISTINCT u.unit_id) AS num_apt
+FROM airbnb_units u
+JOIN airbnb_hosts h
+  ON u.host_id = h.host_id
+WHERE u.unit_type = 'Apartment'
+  AND h.age < 30
+GROUP BY h.nationality
+ORDER BY num_apt DESC;
+
