77df545581f0f778e2d50410047871c8cb54cf1a
Walid Sharaar <walidsharar@gmail.com>
2025-09-28 12:31:25 +0200

    Update Day47.sql

--- Day47.sql
+++ Day47.sql
@@ -15,3 +15,19 @@ WHERE u.unit_type = 'Apartment'
 GROUP BY h.nationality
 ORDER BY num_apt DESC;
 
+--Alternative
+
+WITH filtered_units AS (
+    SELECT * 
+    FROM airbnb_units
+    WHERE unit_type = 'Apartment'
+)
+SELECT 
+    h.nationality,
+    COUNT(DISTINCT u.unit_id) AS num_apt
+FROM filtered_units u
+JOIN airbnb_hosts h
+  ON u.host_id = h.host_id
+WHERE h.age < 30
+GROUP BY h.nationality
+ORDER BY num_apt DESC;
