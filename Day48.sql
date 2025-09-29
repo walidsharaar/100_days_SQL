e2b89f6ba61b98c37940c9bce143101b691efd09
Walid Sharaar <walidsharar@gmail.com>
2025-09-29 12:07:16 +0200

    Create Day48.sql

---
+++ Day48.sql
@@ -0,0 +1,6 @@
+/*
+Identify the most engaged guests by ranking them according to their overall messaging activity. The most active guest, meaning the one who has exchanged the most messages with hosts, should have the highest rank. 
+If two or more guests have the same number of messages, they should have the same rank. Importantly, the ranking shouldn't skip any numbers, even if many guests share the same rank. Present your results in a clear format,
+showing the rank, guest identifier, and total number of messages for each guest, ordered from the most to least active.
+*/
+
