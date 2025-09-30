5f253a87c3b810c68b667659c180aac5c75c73c9
Walid Sharaar <walidsharar@gmail.com>
2025-09-30 08:48:48 +0200

    Create Day49.sql

---
+++ Day49.sql
@@ -0,0 +1,6 @@
+/*
+Count the number of unique street names for each postal code in the business dataset. Use only the first word of the street name, case insensitive (e.g., "FOLSOM" and "Folsom" are the same).
+If the structure is reversed (e.g., "Pier 39" and "39 Pier"), count them as the same street. Output the results with postal codes, 
+ordered by the number of streets (descending) and postal code (ascending).
+*/
+
