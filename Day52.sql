1f557fb6b51a6ce160dfc8a4ba5b113c5db79b68
Walid Sharaar <walidsharar@gmail.com>
2025-10-03 10:06:56 +0200

    Create Day52.sql

---
+++ Day52.sql
@@ -0,0 +1,3 @@
+/*
+Identify returning active users by finding users who made a second purchase within 7 days or less of any previous transaction, excluding same-day purchases. Output a list of these user_id.
+*/
