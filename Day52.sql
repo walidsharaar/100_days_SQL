79e6a1f8e831b8371693b43712e29ab8336dc8ae
Walid Sharaar <walidsharar@gmail.com>
2025-10-03 10:09:38 +0200

    Update Day52.sql

--- Day52.sql
+++ Day52.sql
@@ -1,3 +1,12 @@
 /*
 Identify returning active users by finding users who made a second purchase within 7 days or less of any previous transaction, excluding same-day purchases. Output a list of these user_id.
 */
+
+SELECT DISTINCT a.user_id
+FROM amazon_transactions AS a
+JOIN amazon_transactions AS b
+ON a.user_id = b.user_id
+    AND a.created_at +7 > b.created_at
+    AND a.created_at < b.created_at
+    AND a.created_at <> b.created_at
+ORDER BY a.user_id
