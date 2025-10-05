ab6c7a862e7106fb3e400246297d71769567b1cb
Walid Sharaar <walidsharar@gmail.com>
2025-10-05 09:43:38 +0200

    Create Day54.sql

---
+++ Day54.sql
@@ -0,0 +1,9 @@
+/*
+The election is conducted in a city and everyone can vote for one or more candidates, or choose not to vote at all. Each person has 1 vote so if they vote for multiple candidates, 
+their vote gets equally split across these candidates. For example, if a person votes for 2 candidates, these candidates receive an equivalent of 0.5 vote each. Some voters have chosen not to vote, 
+which explains the blank entries in the dataset.
+Find out who got the most votes and won the election. Output the name of the candidate or multiple names in case of a tie.
+To avoid issues with a floating-point error you can round the number of votes received by a candidate to 3 decimal places.
+
+*/
+
