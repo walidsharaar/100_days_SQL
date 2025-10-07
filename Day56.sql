1bb91b1cdb171a6e3e6e7ea35578261204340688
Walid Sharaar <walidsharar@gmail.com>
2025-10-07 16:32:16 +0200

    Create Day56.sql

---
+++ Day56.sql
@@ -0,0 +1,3 @@
+/*
+Which user flagged the most distinct videos that ended up approved by YouTube? Output, in one column, their full name or names in case of a tie. In the user's full name, include a space between the first and the last name.
+*/
