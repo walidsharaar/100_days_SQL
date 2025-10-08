80a9ea6fec19b33d4e657652308cd15862607e9e
Walid Sharaar <walidsharar@gmail.com>
2025-10-08 09:49:33 +0200

    Update Day57.sql

--- Day57.sql
+++ Day57.sql
@@ -1,3 +1,24 @@
 /*
 Identify the IDs of students who scored exactly at the median for the SAT writing section.
 */
+WITH cte AS (
+    SELECT
+        AVG(sat_writing) AS meadian_sat_writing
+    FROM (
+        SELECT
+            student_id,
+            sat_writing,
+            ROW_NUMBER() OVER (ORDER BY sat_writing ASC, student_id ASC) AS rnk1,
+            ROW_NUMBER() OVER (ORDER BY sat_writing DESC, student_id DESC) AS rnk2
+        FROM
+            sat_scores) AS t1
+    WHERE
+        ABS(rnk1 - rnk2) <= 1
+)
+
+SELECT
+    student_id
+FROM
+    sat_scores
+WHERE
+    sat_writing = (SELECT meadian_sat_writing FROM cte)
