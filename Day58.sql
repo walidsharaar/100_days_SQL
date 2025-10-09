e0344d7628923dc08a4b05bdebc948c94ed63ef8
Walid Sharaar <walidsharar@gmail.com>
2025-10-09 09:05:03 +0200

    Create Day58.sql

---
+++ Day58.sql
@@ -0,0 +1,18 @@
+/*
+You are given a table named airbnb_host_searches that contains data for rental property searches made by users. Determine the minimum, average, and maximum rental prices for each popularity-rating bucket. 
+A popularity rating bucket should be assigned to every record based on its number of reviews (see rules below).
+
+
+The host’s popularity rating is defined as below:
+•   0 reviews: "New"
+•   1 to 5 reviews: "Rising"
+•   6 to 15 reviews: "Trending Up"
+•   16 to 40 reviews: "Popular"
+•   More than 40 reviews: "Hot"
+
+
+Tip: The id column in the table refers to the search ID.
+
+
+Output host popularity rating and their minimum, average and maximum rental prices. Order the solution by the minimum cost.
+*/
