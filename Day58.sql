2840e33bbe78e650d1e12b8cb2ba7340138b2255
Walid Sharaar <walidsharar@gmail.com>
2025-10-09 09:14:32 +0200

    Update Day58.sql

--- Day58.sql
+++ Day58.sql
@@ -16,3 +16,30 @@ Tip: The id column in the table refers to the search ID.
 
 Output host popularity rating and their minimum, average and maximum rental prices. Order the solution by the minimum cost.
 */
+
+WITH host_data AS (
+  SELECT
+    price,
+    number_of_reviews
+  FROM airbnb_host_searches
+),
+pop_ratings AS (
+  SELECT
+    CASE
+      WHEN number_of_reviews = 0 THEN 'New'
+      WHEN number_of_reviews BETWEEN 1 AND 5 THEN 'Rising'
+      WHEN number_of_reviews BETWEEN 6 AND 15 THEN 'Trending Up'
+      WHEN number_of_reviews BETWEEN 16 AND 40 THEN 'Popular'
+      WHEN number_of_reviews > 40 THEN 'Hot'
+    END AS host_popularity,
+    price
+  FROM host_data
+)
+SELECT
+  host_popularity,
+  MIN(price) AS min_price,
+  AVG(price) AS avg_price,
+  MAX(price) AS max_price
+FROM pop_ratings
+GROUP BY host_popularity
+ORDER BY MIN(price);
