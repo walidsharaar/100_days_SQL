5266c0d437f67e19785c158f81d2b0bbe8a5d5ff
Walid Sharaar <walidsharar@gmail.com>
2025-10-10 09:23:57 +0200

    Update Day59.sql

--- Day59.sql
+++ Day59.sql
@@ -2,3 +2,12 @@
 Find the top 10-ranked songs in 2010. Output the rank, group name, and song name, but do not show the same song twice. Sort the result based on the rank in ascending order.
 
 */
+
+
+SELECT DISTINCT(year_rank) AS rank,
+       group_name,
+       song_name
+FROM billboard_top_100_year_end
+WHERE year = 2010
+ORDER BY year_rank ASC
+LIMIT 10;
