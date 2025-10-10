8c8f10672c8d46eaddbb47a97a2d484d6f848700
Walid Sharaar <walidsharar@gmail.com>
2025-10-10 09:22:16 +0200

    Create Day59.sql

---
+++ Day59.sql
@@ -0,0 +1,4 @@
+/*
+Find the top 10-ranked songs in 2010. Output the rank, group name, and song name, but do not show the same song twice. Sort the result based on the rank in ascending order.
+
+*/
