28064e8287a9d4a7304003e3e0719dd591461e45
Walid Sharaar <walidsharar@gmail.com>
2025-10-12 14:55:56 +0200

    Create Day61.sql

---
+++ Day61.sql
@@ -0,0 +1,4 @@
+/*
+Find the best-selling item for each month (no need to separate months by year). The best-selling item is determined by the highest total sales amount, calculated as: total_paid = unitprice * quantity. Output the month, description of the item, and the total amount paid.
+
+*/
