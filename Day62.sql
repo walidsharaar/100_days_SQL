f5c01c28a9c4befa829530c5943bdd9832f38907
Walid Sharaar <walidsharar@gmail.com>
2025-10-13 08:30:51 +0200

    Create Day62.sql

---
+++ Day62.sql
@@ -0,0 +1,4 @@
+/*
+Compare the total number of comments made by users in each country between December 2019 and January 2020. For each month, rank countries by total comments using dense ranking (i.e., avoid gaps between ranks) in descending order.
+Then, return the names of the countries whose rank improved from December to January.
+*/
