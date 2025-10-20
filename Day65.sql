fa19c176208d95dceb46e816dc3a9df4a4561497
Walid Sharaar <walidsharar@gmail.com>
2025-10-20 10:50:03 +0200

    Update Day65.sql

--- Day65.sql
+++ Day65.sql
@@ -1,1 +1,14 @@
 
+/*
+Find the genre of the person with the most number of oscar winnings.
+If there are more than one person with the same number of oscar wins, return the first one in alphabetic order based on their name. Use the names as keys when joining the tables.
+*/
+
+SELECT top_genre
+FROM nominee_information ni
+JOIN oscar_nominees om
+  ON ni.name = om.nominee
+WHERE om.winner = TRUE
+GROUP BY top_genre
+ORDER BY COUNT(*) DESC, top_genre ASC
+LIMIT 1;
