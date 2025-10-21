e107b1552c3cf3d3b92c8e7efa08b5f6340afc14
Walid Sharaar <walidsharar@gmail.com>
2025-10-21 14:47:12 +0200

    Create Day70.sql for marketing campaign analysis
    Add SQL script to analyze marketing campaign impact on user purchases.

---
+++ Day70.sql
@@ -0,0 +1,6 @@
+/*
+You have the marketing_campaign table, which records in-app purchases by users. Users making their first in-app purchase enter a marketing campaign, where they see call-to-actions for more purchases. Find how many users made additional purchases due to the campaign's success.
+
+
+The campaign starts one day after the first purchase. Users with only one or multiple purchases on the first day do not count, nor do users who later buy only the same products from their first day.
+*/
