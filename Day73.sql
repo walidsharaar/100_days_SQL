00f16db70463ab6492e19fa042f7284c94514ae1
Walid Sharaar <walidsharar@gmail.com>
2025-10-26 14:04:02 +0100

    Implement query to find city with most amenities
    Added SQL queries to find the city with the most amenities from Airbnb search data.

--- Day73.sql
+++ Day73.sql
@@ -1,1 +1,39 @@
-/**/
+/*
+
+You're given a dataset of searches for properties on Airbnb. For simplicity, let's say that each search result (i.e., each row) represents a unique host. Find the city with the most
+amenities across all their host's properties. Output the name of the city.
+
+
+*/
+
+
+WITH cte AS (
+  SELECT 
+    city,
+    TRIM(BOTH '{, ", }' FROM amenity) AS split_amenity
+  FROM airbnb_search_details,
+       UNNEST(STRING_TO_ARRAY(amenities, ',')) AS amenity
+)
+SELECT 
+  city
+FROM cte
+GROUP BY city
+ORDER BY COUNT(DISTINCT split_amenity) DESC
+LIMIT 1;
+
+
+--Alternative
+
+WITH cte AS (
+  SELECT 
+    city,
+    TRIM(BOTH '{, ", }' FROM regexp_split_to_table(amenities, ',')) AS amenity
+  FROM airbnb_search_details
+)
+SELECT 
+  city,
+  COUNT(DISTINCT amenity) AS unique_amenities
+FROM cte
+GROUP BY city
+ORDER BY unique_amenities DESC
+LIMIT 1;
