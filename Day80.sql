5c24d337dfc1b1b3f8c41c1d491f148c636893b7
sharaar <walidsharar@gmail.com>
2025-11-01 09:44:13 +0100

    Add new challenge

---
+++ Day80.sql
@@ -0,0 +1,23 @@
+/*
+Find the top 3 doctors (doctor_id) across all hospitals who meet the following conditions:
+
+They have handled at least 30 patient visits.
+
+Their average treatment success rate is above the hospital average.
+
+Return:
+
+doctor_id
+
+hospital_id
+
+total_patients
+
+success_rate
+
+avg_feedback
+
+performance_score
+
+rank (1 = highest performer)
+*/
