d255fae54a30c99f618cd1478c2120b554cf79da
sharaar <walidsharar@gmail.com>
2025-11-05 01:48:43 +0100

    Add new challenge for today

---
+++ Day84.sql
@@ -0,0 +1,6 @@
+/*
+You work for FinEdge Bank, which offers personal and business accounts.
+The BI team wants to understand how effective relationship managers (RMs) are at retaining and growing client engagement — measured by transaction volume and activity consistency.
+Identify top 3 relationship managers (RMs) per account_type who achieved the highest growth in client engagement between Q1 2024 and Q2 2024
+A client is active in a quarter if they made ≥2 transactions.
+*/
