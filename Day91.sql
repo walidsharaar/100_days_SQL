55e929d1f3037b15e0e2ae032a3cd363c775807f
Walid Sharaar <walidsharar@gmail.com>
2025-11-12 09:55:14 +0100

    Create Day91.sql

---
+++ Day91.sql
@@ -0,0 +1,10 @@
+/*
+You’re working for a digital bank. Management wants to understand how client engagement, account activity, and retention spending evolve month by month,
+to predict when clients are at the highest churn risk and whether retention spending is working.
+For each country and account_type, calculate:
+Monthly churn rate over the last 12 months.
+Average monthly retention spend per churned client.
+Rolling 3-month moving average churn rate to show trends.
+Identify the month and segment (country + account_type) where churn peaked.
+
+*/
