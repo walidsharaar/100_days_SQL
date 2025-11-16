10892e2281568b0669e6f45f53f4db3813ac5a63
Walid Sharaar <walidsharar@gmail.com>
2025-11-16 12:43:13 +0100

    Add SQL query for customer default analysis
    Analyze customer repayment behavior to identify default risks.

---
+++ Day95.sql
@@ -0,0 +1,11 @@
+/*
+You are working as a Data Analyst in a digital bank that provides personal loans.
+Your goal is to analyze customer repayment behavior and detect early signs of cashflow risk.
+Find the top 3 states where customers are most likely to default, defined as:
+having 2 or more late payments within the last 90 days from the current date, and
+belonging to “Medium” or “High” risk segments.
+Also calculate for each state:
+Total number of risky customers
+Average loan amount of those customers
+Late payment rate (late_payments / total_payments)
+*/
