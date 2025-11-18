3c6690d4be1870dc5cd83fdd58b245d709836c1b
sharaar <walidsharar@gmail.com>
2025-11-18 09:35:38 +0100

    Change question text

--- Day94.sql
+++ Day94.sql
@@ -1,17 +1,17 @@
 /*
 You’re working as a Senior BI Engineer for an e-commerce platform running multiple ad campaigns across channels (Google, Meta, Email, etc.).
 The CMO wants to evaluate which campaign drives the best ROI, funnel retention, and long-term conversions — not just click-throughs.
-You need to:
-Build a 3-step funnel per campaign:
-Step 1: Clicks
-Step 2: Signups
-Step 3: Purchases
-Calculate the conversion rate between each funnel stage.
-Compute ROI = (Total Revenue - Total Spend) / Total Spend per campaign.
-Identify top 3 campaigns that have:
-ROI > 1 (profitable)
-AND at least 50% signup-to-purchase conversion rate
-Output: campaign_id, channel, click_to_signup_rate, signup_to_purchase_rate, ROI
+You need to
+Build a 3-step funnel per campaign:
+Step 1: Clicks
+Step 2: Signups
+Step 3: Purchases
+Calculate the conversion rate between each funnel stage.
+Compute ROI = (Total Revenue - Total Spend) / Total Spend per campaign.
+Identify top 3 campaigns that have:
+ROI > 1 (profitable)
+AND at least 50% signup-to-purchase conversion rate
+Output:campaign_id, channel, click_to_signup_rate, signup_to_purchase_rate, ROI
 */
 
 
